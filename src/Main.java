c768f810a280b2fded668407645e1e332df63c53
juespega <juespega666@gmail.com>
2024-10-06 18:57:36 -0500

    second version of the banking application

--- src/Main.java
+++ src/Main.java
@@ -4,57 +4,61 @@ public class Main {
 
 
     public static void main(String[] args) {
-
+        // Create an instance of the Scanner class to get user input
         Scanner scanner = new Scanner(System.in);
 
-        String nombre = "Tony Stark";
-        String tipoDeCuenta = "Corriente";
-        double saldo = 1599.99;
-        int opcion = 0;
+        // Definition of variables
+        String name = "Tony Stark";
+        String accountType = "Current";
+        double balance = 1599.99;
+        int option = 0;
 
+        // Main customer data
         System.out.println("********************");
-        System.out.println("\nNombre del cliente: " + nombre);
-        System.out.println("Tipo de cuenta: " + tipoDeCuenta);
-        System.out.println("Su saldo disponible es: $" + saldo);
+        System.out.println("\nCustomer Name: " + name);
+        System.out.println("Account Type: " + accountType);
+        System.out.println("Your available balance is: $" + balance);
         System.out.println("\n********************");
 
+        // Menu creation
         String menu = """
-                *** Escriba el número de la opción deseada ***
-                1 - Consultar saldo
-                2 - Retirar
-                3 - Depositar
-                9 - Salir
+                *** Enter the number of the desired option ***
+                1 - Check balance
+                2 - Withdraw money
+                3 - Deposit money
+                9 - Exit
                 """;
 
-        while (opcion != 9){
+        // Application logic (transactions)
+        while (option != 9){
             System.out.println(menu);
-            opcion = scanner.nextInt();
+            option = scanner.nextInt();
 
-            switch (opcion){
+            switch (option){
                 case 1:
-                    System.out.println("El saldo actualizado es: $"+ saldo);
+                    System.out.println("The updated balance is: $"+ balance);
                     break;
                 case 2:
-                    System.out.println("¿Cuánto dinero desea retirar?");
-                    double valorARetirar = scanner.nextDouble();
-                    if (saldo < valorARetirar){
-                        System.out.println("Saldo Insuficiente");
+                    System.out.println("How much money do you want to withdraw?");
+                    double valueToWithdraw = scanner.nextDouble();
+                    if (balance < valueToWithdraw){
+                        System.out.println("Insufficient balance");
                     } else{
-                        saldo = saldo - valorARetirar;
-                        System.out.println("El saldo actualizado es: $"+ saldo);
+                        balance = balance - valueToWithdraw;
+                        System.out.println("The updated balance is: $"+ balance);
                     }
                     break;
                 case 3:
-                    System.out.println("¿Cuánto dinero desea depositar?");
-                    double valorADepositar = scanner.nextDouble();
-                    saldo += valorADepositar;
-                    System.out.println("El saldo actualizado es: $"+ saldo);
+                    System.out.println("How much money do you want to deposit?");
+                    double amountToDeposit = scanner.nextDouble();
+                    balance += amountToDeposit;
+                    System.out.println("The updated balance is: $"+ balance);
                     break;
                 case 9:
-                    System.out.println("Saliendo del programa, gracias por utilizar nuestros servicios.");
+                    System.out.println("Leaving the program, thank you for using our services.");
                     break;
                 default:
-                    System.out.println("Opción no valida");
+                    System.out.println("Invalid option");
             }
         }
     }
